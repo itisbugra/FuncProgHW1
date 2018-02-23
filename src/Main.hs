94330ecfb223aed69088163134e0815281685d07
Buğra Ekuklu <ekuklu@icloud.com>
2018-02-23 03:29:13 +0300

    Added cabal build system.

---
+++ src/Main.hs
@@ -0,0 +1,44 @@
+{- |
+Module      :  Main
+Description :  Application start point.
+Copyright   :  (c) 2018 Buğra Ekuklu (chatatata)
+License     :  See LICENSE.md
+
+Maintainer  :  ekuklu[at]icloud.com
+Stability   :  experimental
+Portability :  portable
+
+Implements *Zeller's congruence* in order to calculated the day of the week on given day, month
+and year in Gregorian calendar.
+-}
+module Main where
+
+import Prelude
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
+
+-- | Size of a century.
+centurySize :: Integer
+centurySize = 100
+
+-- | Data type representing day of a week.
+data DayOfWeek = 
+  Saturday | Sunday | Monday | Tuesday | Wednesday | Thursday | Friday  deriving (Enum)
+
+-- | Calculates day of a week on a Gregorian calendar by using *Zeller's congruence* method.
+dayOfWeek :: Integer -- ^ Day of the month, e.g. 21.
+          -> Integer -- ^ Month of the year.
+          -> Integer -- ^ Year.
+          -> Integer -- ^ Calculated day of the week.
+dayOfWeek day month year = 
+    calculatedDay `mod` 7 where
+  yearOfCentury :: Integer
+  yearOfCentury = year `mod` centurySize
+
+  zeroBasedCentury :: Integer
+  zeroBasedCentury = floor(fromInteger(year) / fromInteger(centurySize))
+
+  calculatedDay :: Integer
+  calculatedDay = month + yearOfCentury + (realToFrac(zeroBasedCentury) / realToFrac(4.00))
+
